cc923c2293669198f7aed60ecc1d0f6e4b3b581b
mizlin <miz1205@gmail.com>
2021-04-08 23:30:56 +0800

    Day 2 5. Longest Palindromic Substring 11. Container With Most Water

---
+++ code/11. Container With Most Water.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <cassert>
+#include <stack>
+#include <vector>
+
+using std::vector;
+using std::stack;
+class Solution {
+public:
+    int maxArea(vector<int>& height)
+    {
+        int maxArea = 0;
+        int i = 0;
+        int j = height.size() - 1;
+        while (i < j) {
+            if (height[i] > height[j]) {
+                maxArea = std::max(maxArea, (j - i) * height[j]);
+                --j;
+            } else {
+                maxArea = std::max(maxArea, (j - i) * height[i]);
+                ++i;
+            }
+        }
+        return maxArea;
+    }
+};
+
+int main()
+{
+    Solution s;
+    {
+        vector<int> input{ 1,8,6,2,5,4,8,3,7 };
+        int ans = 49;
+        assert(ans == s.maxArea(input));
+    }
+    {
+        vector<int> input{ 1,1 };
+        int ans = 1;
+        assert(ans == s.maxArea(input));
+    }
+    {
+        vector<int> input{ 4,3,2,1,4 };
+        int ans = 16;
+        assert(ans == s.maxArea(input));
+    }
+    {
+        vector<int> input{ 1,2,1 };
+        int ans = 2;
+        assert(ans == s.maxArea(input));
+    }
+    return 0;
+}
---
+++ code/5. Longest Palindromic Substring.cpp
@@ -0,0 +1,83 @@
+#include <cassert>
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    string longestPalindrome(string s)
+    {
+        if (s.size() < 2) {
+            return s.substr(0, 1);
+        }
+        int maxC = 0;
+        int maxW = 0;
+        string paddedString = addPadding(s);
+        for (int i = 0; i < paddedString.size(); ++i) {
+            int width = 1;
+            while ((i - width >= 0) && (i + width < paddedString.size()) && (paddedString[i - width] == paddedString[i + width])) {
+                ++width;
+            }
+            --width;
+            if (width > maxW) {
+                maxW = width;
+                maxC = i;
+            }
+        }
+        return removePadding(paddedString.substr(maxC - maxW, 2 * maxW + 1));
+    }
+
+private:
+    string addPadding(const string& s)
+    {
+        string paddedString;
+        for (char c : s) {
+            paddedString += '*';
+            paddedString += c;
+        }
+        paddedString += '*';
+        return paddedString;
+    }
+
+    string removePadding(const string& s)
+    {
+        string unpaddedString;
+        for (char c : s) {
+            if (c != '*') {
+                unpaddedString += c;
+            }
+        }
+        return unpaddedString;
+    }
+};
+
+int main()
+{
+    Solution s;
+    {
+        string input = "babad";
+        string ans = "bab";
+        assert(ans == s.longestPalindrome(input));
+    }
+    {
+        string input = "cbbd";
+        string ans = "bb";
+        assert(ans == s.longestPalindrome(input));
+    }
+    {
+        string input = "a";
+        string ans = "a";
+        assert(ans == s.longestPalindrome(input));
+    }
+    {
+        string input = "ac";
+        string ans = "a";
+        assert(ans == s.longestPalindrome(input));
+    }
+    {
+        string input = "bb";
+        string ans = "bb";
+        assert(ans == s.longestPalindrome(input));
+    }
+    return 0;
+}
