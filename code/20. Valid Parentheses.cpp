96bc49eb83ac64a5398a8d7d7c1c9833f3242747
mizlin <miz1205@gmail.com>
2021-04-15 00:12:34 +0800

    Day 4 20. Valid Parentheses

---
+++ code/20. Valid Parentheses.cpp
@@ -0,0 +1,72 @@
+#include <cassert>
+#include <stack>
+#include <string>
+#include <unordered_map>
+
+using std::stack;
+using std::string;
+using std::unordered_map;
+
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        static const unordered_map<char, char> close2Open {
+            {')', '('}, {']', '['}, {'}', '{'}
+        };
+        stack<char> st;
+        for (auto& c : s) {
+            auto it = close2Open.find(c);
+            if (it == close2Open.end()) {
+                st.emplace(c);
+            } else {
+                if (st.empty() || it->second != st.top()) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
+int main()
+{
+    Solution s;
+    {
+        string input = "()";
+        bool ans = true;
+        assert(ans == s.isValid(input));
+    }
+    {
+        string input = "()[]{}";
+        bool ans = true;
+        assert(ans == s.isValid(input));
+    }
+    {
+        string input = "(]";
+        bool ans = false;
+        assert(ans == s.isValid(input));
+    }
+    {
+        string input = "([)]";
+        bool ans = false;
+        assert(ans == s.isValid(input));
+    }
+    {
+        string input = "{[]}";
+        bool ans = true;
+        assert(ans == s.isValid(input));
+    }
+    {
+        string input = "[";
+        bool ans = false;
+        assert(ans == s.isValid(input));
+    }
+    {
+        string input = "]";
+        bool ans = false;
+        assert(ans == s.isValid(input));
+    }
+    return 0;
+}
