9bb336874db9fe46521af55c3e877304ef2b3aef
mizlin <miz1205@gmail.com>
2021-05-11 23:20:34 +0800

    Day 8 39. Combination Sum 48. Rotate Image

---
+++ code/39. Combination Sum.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
+    {
+        ans.clear();
+        std::sort(candidates.begin(), candidates.end());
+        vector<int> tmp;
+        func(candidates, target, 0, tmp);
+        return ans;
+    }
+
+private:
+    vector<vector<int>> ans;
+
+    void func(vector<int>& candidates, int target, int index, vector<int>& tmp)
+    {
+        for (size_t i = index; i < candidates.size(); ++i) {
+            if (target - candidates[i] == 0) {
+                tmp.emplace_back(candidates[i]);
+                ans.emplace_back(tmp);
+                tmp.pop_back();
+                break;
+            } else if (target - candidates[i] > 0) {
+                tmp.emplace_back(candidates[i]);
+                func(candidates, target - candidates[i], i, tmp);
+                tmp.pop_back();
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+    {
+        vector<int> input{ 2 };
+        int target = 1;
+        vector<vector<int>> output = s.combinationSum(input, target);
+        vector<vector<int>> ans{};
+        assert(ans == output);
+    }
+    {
+        vector<int> input{ 1 };
+        int target = 1;
+        vector<vector<int>> output = s.combinationSum(input, target);
+        vector<vector<int>> ans{ { 1 } };
+        assert(ans == output);
+    }
+    {
+        vector<int> input{ 1 };
+        int target = 2;
+        vector<vector<int>> output = s.combinationSum(input, target);
+        vector<vector<int>> ans{ { 1, 1 } };
+        assert(ans == output);
+    }
+    {
+        vector<int> input{ 2,3,6,7 };
+        int target = 7;
+        vector<vector<int>> output = s.combinationSum(input, target);
+        vector<vector<int>> ans{ { 2, 2, 3 },{ 7 } };
+        assert(ans == output);
+    }
+    return 0;
+}
---
+++ code/48. Rotate Image.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <cassert>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix)
+    {
+        flipRowCol(matrix);
+        flipCol(matrix);
+    }
+private:
+    void swap(int& a, int& b)
+    {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+
+    void flipRowCol(vector<vector<int>>& matrix)
+    {
+        size_t n = matrix.size();
+        for (size_t row = 0; row < n; ++row) {
+            for (size_t col = row + 1; col < n; ++col) {
+                swap(matrix[row][col], matrix[col][row]);
+            }
+        }
+    }
+
+    void flipCol(vector<vector<int>>& matrix)
+    {
+        size_t n = matrix.size();
+        for (size_t row = 0; row < n; ++row) {
+            for (size_t col = 0; col < n / 2; ++col) {
+                swap(matrix[row][col], matrix[row][n - col - 1]);
+            }
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+    {
+        vector<vector<int>> input{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        s.rotate(input);
+        vector<vector<int>> ans{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+        assert(ans == input);
+    }
+    {
+        vector<vector<int>> input{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        s.rotate(input);
+        vector<vector<int>> ans{{15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}};
+        assert(ans == input);
+    }
+    {
+        vector<vector<int>> input{ {1} };
+        s.rotate(input);
+        vector<vector<int>> ans{ {1} };
+        assert(ans == input);
+    }
+    {
+        vector<vector<int>> input{{1, 2}, {3, 4}};
+        s.rotate(input);
+        vector<vector<int>> ans{{3, 1}, {4, 2}};
+        assert(ans == input);
+    }
+    return 0;
+}
